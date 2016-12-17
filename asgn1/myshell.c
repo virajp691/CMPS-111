f59f7600d2555777af33450fa5b9c0d60354cfd0
Viraj Patel <vispatel@ucsc.edu>
2016-12-16 21:57:09 -0800

    Started to build a basic shell

---
+++ asgn1/myshell.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <errno.h>
+
+extern char **getline();
+
+int main() {
+	int i;
+	char **args;
+
+	while(1) {
+		args = getline();
+		for(i = 0; args[i] != NULL; i++) {
+			printf("Argument %d: %s\n", i, args[i]);
+		}
+	}
+}
+
